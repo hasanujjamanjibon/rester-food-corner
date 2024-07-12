6984345a2b7cd7e5babd931654f5ad385c4d22bc
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2024-07-12 16:13:47 +0600

    swiper slide added

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,4 +6,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
     menuIcon.classList.toggle("change");
     navLinks.classList.toggle("show");
   });
+
+  /*  ================
+  ! ==>    Swiper slider 
+  =================== */
+  const slides = document.querySelector(".my__slides");
+  const slide = document.querySelector(".my__slide");
+  const nextBtn = document.querySelector(".nextBtn");
+  const prevBtn = document.querySelector(".prevBtn");
+  const slideWidth = slide.clientWidth;
+  const totalSlides = document.querySelectorAll(".my__slide").length;
+  let currentIndex = 0;
+
+  function updateSlidePosition() {
+    slides.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+  }
+
+  prevBtn.addEventListener("click", function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateSlidePosition();
+  });
+  nextBtn.addEventListener("click", function prevSlide() {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    updateSlidePosition();
+  });
 });
