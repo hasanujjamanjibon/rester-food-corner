d36722ffd4dce3fba44ba903435314f9eafb055e
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2024-07-13 00:04:27 +0600

    about section added & some awesome scale up animation added & all section are responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,26 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  const sections = document.querySelectorAll(".animation");
+
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5, // Adjust this value to change the visibility threshold
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("hidden");
+      } else {
+        entry.target.classList.add("hidden");
+      }
+    });
+  }, observerOptions);
+
+  sections.forEach((section) => {
+    section.classList.add("hidden"); // Initialize all sections as hidden
+    observer.observe(section);
+  });
   /*
    * ************************
    * ==>    Nav Menu
