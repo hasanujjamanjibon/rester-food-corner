8883970a4bf24d972d65de40fa56e37426dc9e55
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2024-07-13 13:04:47 +0600

    2nd section swiper slider added & device responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
       }
     });
   }, observerOptions);
-
   sections.forEach((section) => {
     section.classList.add("hidden"); // Initialize all sections as hidden
     observer.observe(section);
@@ -32,33 +31,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
   menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("change");
     navLinks.classList.toggle("show");
-  });
-
-  /*
-   * ************************
-   * ==>    Swiper slider
-   * ************************
-   */
-  const slides = document.querySelector(".my__slides");
-  const slide = document.querySelector(".my__slide");
-  const nextBtn = document.querySelector(".nextBtn");
-  const prevBtn = document.querySelector(".prevBtn");
-  const slideWidth = slide.clientWidth;
-  const totalSlides = document.querySelectorAll(".my__slide").length;
-  console.log(totalSlides);
-  let currentIndex = 0;
-
-  function updateSlidePosition() {
-    slides.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-    console.log(currentIndex * slideWidth, "px");
-  }
-
-  nextBtn.addEventListener("click", function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    updateSlidePosition();
-  });
-  prevBtn.addEventListener("click", function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    updateSlidePosition();
   });
 });
